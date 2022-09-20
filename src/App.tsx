3447437840e2b8c14314bb6472af9eac207247e6
behzadsoleimani <behzadsoleimani97@gmail.com>
2022-09-20 11:27:08 +0430

    remove additional files

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createUseStyles } from 'react-jss'
+import DropDown from './components/dropdown';
+
+
+const useStyles = createUseStyles({
+  app: {
+    textAlign: 'center',
+  }
+});
 
 function App() {
+  
+
+  const classes = useStyles()
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className={classes.app}>
+      <DropDown />
+    </main>
   );
 }
 
